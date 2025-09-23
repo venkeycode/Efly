6d1202b7d4e0b989af2ebb05d28cf9946609978d
Venkey <venkey.code@gmail.com>
2025-09-23 12:20:38 +0530

    code

---
+++ database/migrations/2025_09_23_121947_add_wallet_address_to_customdetails_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('mmm5', function (Blueprint $table) {
+           $table->string('wallet_address')->nullable()->after('dob');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('mmm5', function (Blueprint $table) {
+            //
+        });
+    }
+};
