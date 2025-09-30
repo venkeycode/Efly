e9383cf65ede9b5acd5b40790d3508bc66c0cfc0
Venkey <venkey.code@gmail.com>
2025-09-30 12:48:04 +0530

    withdraw

--- app/Models/CryptoPayout.php
+++ app/Models/CryptoPayout.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class CryptoPayout extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'token',
+        'chain',
+        'from',
+        'to',
+        'amount',
+        'amount_wei',
+        'tx_hash',
+        'status',
+        'reference',
+    ];
 }
